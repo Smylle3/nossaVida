1744c2692ea5d4579c431e819d774bc95d2ac2b0
Smylle3 <joao21731@gmail.com>
2023-11-26 21:32:48 -0300

    fix .env config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-	databaseURL: process.env.REACT_APP_DATABASE_URL
+	apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+	authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_REACT_APP_APP_ID,
+	measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
+	databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
