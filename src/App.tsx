c085e2e15a6cf23c8d1925dfebc517b820790833
Smylle3 <joao21731@gmail.com>
2023-11-28 16:31:58 -0300

    Set routes on vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,30 @@ import { Home } from './pages/Home';
 import { AppProvider } from './context/myContext';
 import { Signup } from './pages/Signup';
 import { AuthProvider } from './context/authContext';
-//import PublicRoutes from './routes/PublicRoutes';
-//import PrivateRoutes from './routes/PrivateRoutes';
+import PublicRoutes from './routes/PublicRoutes';
+import PrivateRoutes from './routes/PrivateRoutes';
 
 export default function App() {
 	return (
 		<AuthProvider>
 			<AppProvider>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/signup" element={<Signup />} />
+					<Route
+						path="/"
+						element={
+							<PrivateRoutes>
+								<Home />
+							</PrivateRoutes>
+						}
+					/>
+					<Route
+						path="/signup"
+						element={
+							<PublicRoutes>
+								<Signup />
+							</PublicRoutes>
+						}
+					/>
 				</Routes>
 			</AppProvider>
 		</AuthProvider>
